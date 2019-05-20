de15d8b334d29099c277676792abcba5d894868b
Adam Wathan <adam.wathan@gmail.com>
2019-05-20 09:28:04 -0400

    Polyfill CSS variables for IE 11

--- postcss.config.js
+++ postcss.config.js
@@ -8,6 +8,7 @@ module.exports = {
     require('postcss-import'),
     require('tailwindcss'),
     require('postcss-nested'),
+    require('postcss-custom-properties'),
     require('autoprefixer'),
   ]
 }
