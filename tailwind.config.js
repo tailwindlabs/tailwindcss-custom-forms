a56cae76dd2b498f427d68365f89e947298aa567
Adam Wathan <adam.wathan@gmail.com>
2019-05-20 09:29:17 -0400

    Give select chevron explicit height to ensure correct display in IE 11

--- tailwind.config.js
+++ tailwind.config.js
@@ -99,7 +99,7 @@ module.exports = {
           backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='${theme('colors.gray.500', defaultTheme.colors.gray[500]).replace('#', '%23')}' viewBox='0 0 24 24' %3E%3Cpath d='M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z'/%3E%3C/svg%3E")`,
           backgroundPosition: `right ${theme('spacing.2', defaultTheme.spacing[2])} center`,
           backgroundRepeat: 'no-repeat',
-          backgroundSize: '1.5em',
+          backgroundSize: '1.5em 1.5em',
           '&::-ms-expand': {
             display: 'none',
           },
