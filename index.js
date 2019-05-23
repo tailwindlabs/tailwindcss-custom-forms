5171eb3d3dc0efbc1c5c6dbe9711f48f08016f9b
Adam Wathan <adam.wathan@gmail.com>
2019-05-23 22:31:47 +0200

    Use box-shadow instead of border for checkbox/radios

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const svgToDataUri = require('mini-svg-data-uri')
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/resolveConfig')(require('tailwindcss/defaultConfig')).theme
 
 module.exports = function ({ addUtilities, addComponents, theme }) {
 
@@ -16,8 +16,8 @@ module.exports = function ({ addUtilities, addComponents, theme }) {
     focusShadow: defaultTheme.boxShadow.outline,
     checkboxSize: '1em',
     radioSize: '1em',
-    checkboxIcon: `<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#fff"><path d="M13.293 4.293a1 1 0 0 1 0 1.414L7 12a1 1 0 0 1-1.414 0L3.293 9.707a1 1 0 0 1 1.414-1.414l1.586 1.586 5.586-5.586a1 1 0 0 1 1.414 0z"/></svg>`,
-    radioIcon: `<svg viewBox="0 0 24 24" fill="#fff" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="5"/></svg>`,
+    checkboxIcon: `<svg viewBox="0 0 14 14" fill="#fff" xmlns="http://www.w3.org/2000/svg"><path d="M4.707 6.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L6 7.586 4.707 6.293z"/></svg>`,
+    radioIcon: `<svg viewBox="0 0 14 14" fill="#fff" xmlns="http://www.w3.org/2000/svg"><circle cx="7" cy="7" r="3"/></svg>`,
     checkedColor: 'currentColor',
     selectIcon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="${defaultTheme.colors.gray[500]}"><path d="M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z"/></svg>`,
     selectIconOffset: defaultTheme.spacing[2],
@@ -32,25 +32,28 @@ module.exports = function ({ addUtilities, addComponents, theme }) {
       height: options.checkboxSize,
       width: options.checkboxSize,
       verticalAlign: 'middle',
-      borderColor: options.borderColor,
-      borderWidth: options.borderWidth,
+      boxShadow: `inset 0 0 0 ${options.borderWidth} ${options.borderColor}`,
       borderRadius: options.borderRadius,
       backgroundColor: options.backgroundColor,
       userSelect: 'none',
       '&:focus': {
         outline: 'none',
-        boxShadow: options.focusShadow,
+        boxShadow: `inset 0 0 0 ${options.borderWidth} ${options.borderColor}, ${options.focusShadow}`,
       },
       '&:focus:not(:checked)': {
-        borderColor: options.focusBorderColor,
+        boxShadow: `inset 0 0 0 ${options.borderWidth} ${options.focusBorderColor}, ${options.focusShadow}`,
       },
       '&:checked': {
+        boxShadow: 'none',
         backgroundColor: options.checkedColor,
         borderColor: options.checkedColor,
         backgroundImage: `url("${svgToDataUri(options.checkboxIcon)}")`,
-        backgroundSize: 'cover',
+        backgroundSize: '100% 100%',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
+      },
+      '&:focus:checked': {
+        boxShadow: `${options.focusShadow}`,
       },
       '&::-ms-check': {
         color: 'transparent', // Hide the check
@@ -66,25 +69,27 @@ module.exports = function ({ addUtilities, addComponents, theme }) {
       height: options.radioSize,
       width: options.radioSize,
       verticalAlign: 'middle',
-      borderColor: options.borderColor,
-      borderWidth: options.borderWidth,
+      boxShadow: `inset 0 0 0 ${options.borderWidth} ${options.borderColor}`,
       borderRadius: '50%',
       backgroundColor: options.backgroundColor,
       userSelect: 'none',
       '&:focus': {
         outline: 'none',
-        boxShadow: options.focusShadow,
+        boxShadow: `inset 0 0 0 ${options.borderWidth} ${options.borderColor}, ${options.focusShadow}`,
       },
       '&:focus:not(:checked)': {
-        borderColor: options.focusBorderColor,
+        boxShadow: `inset 0 0 0 ${options.borderWidth} ${options.focusBorderColor}, ${options.focusShadow}`,
       },
       '&:checked': {
+        boxShadow: 'none',
         backgroundColor: options.checkedColor,
-        borderColor: options.checkedColor,
         backgroundImage: `url("${svgToDataUri(options.radioIcon)}")`,
-        backgroundSize: 'cover',
+        backgroundSize: '100% 100%',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
+      },
+      '&:focus:checked': {
+        boxShadow: `${options.focusShadow}`,
       },
       '&::-ms-check': {
         color: 'transparent', // Hide the dot
