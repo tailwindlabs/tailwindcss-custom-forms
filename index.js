325da70117f11c6d8c014e30dbee251cd45eb704
Adam Wathan <adam.wathan@gmail.com>
2019-05-29 14:47:52 +0200

    Use blue as default checkbox/radio button color
    Since it's closer to the expected browser default.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ module.exports = function ({ addUtilities, addComponents, theme }) {
     radioSize: '1em',
     checkboxIcon: `<svg viewBox="0 0 16 16" fill="#fff" xmlns="http://www.w3.org/2000/svg"><path d="M5.707 7.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L7 8.586 5.707 7.293z"/></svg>`,
     radioIcon: `<svg viewBox="0 0 16 16" fill="#fff" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="3"/></svg>`,
-    checkedColor: defaultTheme.colors.gray[800],
+    checkedColor: defaultTheme.colors.blue[500],
     selectIcon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="${defaultTheme.colors.gray[500]}"><path d="M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z"/></svg>`,
     selectIconOffset: defaultTheme.spacing[2],
     selectIconSize: '1.5em',
