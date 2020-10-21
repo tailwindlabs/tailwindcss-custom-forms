f5f6a6cc1440fc7c7bada5bd4f63a46ab3d01968
Robin Malfait <malfait.robin@gmail.com>
2020-10-21 13:16:56 +0200

    port Tailwind UI defaults

--- src/defaultOptions.js
+++ src/defaultOptions.js
@@ -6,9 +6,9 @@ module.exports = {
   input: {
     appearance: 'none',
     backgroundColor: defaultTheme.colors.white,
-    borderColor: defaultTheme.borderColor.DEFAULT,
+    borderColor: defaultTheme.colors.gray[300],
     borderWidth: defaultTheme.borderWidth.DEFAULT,
-    borderRadius: defaultTheme.borderRadius.DEFAULT,
+    borderRadius: defaultTheme.borderRadius.md,
     paddingTop: defaultTheme.spacing[2],
     paddingRight: defaultTheme.spacing[3],
     paddingBottom: defaultTheme.spacing[2],
@@ -16,21 +16,21 @@ module.exports = {
     fontSize: baseFontSize,
     lineHeight: baseLineHeight,
     '&::placeholder': {
-      color: defaultTheme.colors.gray[500],
-      opacity: '1',
+      color: defaultTheme.colors.gray[400],
+      // opacity: '1',
     },
     '&:focus': {
       outline: 'none',
-      boxShadow: defaultTheme.boxShadow.outline,
-      borderColor: defaultTheme.colors.blue[400],
+      boxShadow: defaultTheme.boxShadow.outline, // TODO: Will box-shadow outlines be moved?
+      borderColor: defaultTheme.colors.blue[300],
     },
   },
   textarea: {
     appearance: 'none',
     backgroundColor: defaultTheme.colors.white,
-    borderColor: defaultTheme.borderColor.DEFAULT,
+    borderColor: defaultTheme.colors.gray[300],
     borderWidth: defaultTheme.borderWidth.DEFAULT,
-    borderRadius: defaultTheme.borderRadius.DEFAULT,
+    borderRadius: defaultTheme.borderRadius.md,
     paddingTop: defaultTheme.spacing[2],
     paddingRight: defaultTheme.spacing[3],
     paddingBottom: defaultTheme.spacing[2],
@@ -38,21 +38,21 @@ module.exports = {
     fontSize: baseFontSize,
     lineHeight: baseLineHeight,
     '&::placeholder': {
-      color: defaultTheme.colors.gray[500],
+      color: defaultTheme.colors.gray[400],
       opacity: '1',
     },
     '&:focus': {
       outline: 'none',
-      boxShadow: defaultTheme.boxShadow.outline,
-      borderColor: defaultTheme.colors.blue[400],
+      boxShadow: defaultTheme.boxShadow.outline, // TODO: Will box-shadow outlines be moved?
+      borderColor: defaultTheme.colors.blue[300],
     },
   },
   multiselect: {
     appearance: 'none',
     backgroundColor: defaultTheme.colors.white,
-    borderColor: defaultTheme.borderColor.DEFAULT,
+    borderColor: defaultTheme.colors.gray[300],
     borderWidth: defaultTheme.borderWidth.DEFAULT,
-    borderRadius: defaultTheme.borderRadius.DEFAULT,
+    borderRadius: defaultTheme.borderRadius.md,
     paddingTop: defaultTheme.spacing[2],
     paddingRight: defaultTheme.spacing[3],
     paddingBottom: defaultTheme.spacing[2],
@@ -61,13 +61,14 @@ module.exports = {
     lineHeight: baseLineHeight,
     '&:focus': {
       outline: 'none',
-      boxShadow: defaultTheme.boxShadow.outline,
-      borderColor: defaultTheme.colors.blue[400],
+      boxShadow: defaultTheme.boxShadow.outline, // TODO: Will box-shadow outlines be moved?
+      borderColor: defaultTheme.colors.blue[300],
     },
   },
   select: {
     appearance: 'none',
     colorAdjust: 'exact',
+    // TODO: Still needed now that IE11 is dropped?
     '&::-ms-expand': {
       border: 'none', // The select padding is causing some whitespace around the chevron which looks weird with a border
       '@media not print': {
@@ -76,9 +77,9 @@ module.exports = {
     },
     backgroundRepeat: 'no-repeat',
     backgroundColor: defaultTheme.colors.white,
-    borderColor: defaultTheme.borderColor.DEFAULT,
+    borderColor: defaultTheme.colors.gray[300],
     borderWidth: defaultTheme.borderWidth.DEFAULT,
-    borderRadius: defaultTheme.borderRadius.DEFAULT,
+    borderRadius: defaultTheme.borderRadius.md,
     paddingTop: defaultTheme.spacing[2],
     paddingRight: defaultTheme.spacing[10],
     paddingBottom: defaultTheme.spacing[2],
@@ -87,18 +88,19 @@ module.exports = {
     lineHeight: baseLineHeight,
     backgroundPosition: `right ${defaultTheme.spacing[2]} center`,
     backgroundSize: `1.5em 1.5em`,
-    iconColor: defaultTheme.colors.gray[500],
+    iconColor: defaultTheme.colors.gray[400],
     icon: (iconColor) =>
-      `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="${iconColor}"><path d="M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z"/></svg>`,
+      `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="none"><path d="M7 7l3-3 3 3m0 6l-3 3-3-3" stroke="${iconColor}" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>`,
     '&:focus': {
       outline: 'none',
-      boxShadow: defaultTheme.boxShadow.outline,
-      borderColor: defaultTheme.colors.blue[400],
+      boxShadow: defaultTheme.boxShadow.outline, // TODO: Will box-shadow outlines be moved?
+      borderColor: defaultTheme.colors.blue[300],
     },
   },
   checkbox: {
     appearance: 'none',
     colorAdjust: 'exact',
+    // TODO: Still needed now that IE11 is dropped?
     '&::-ms-check': {
       '@media not print': {
         color: 'transparent', // Hide the check
@@ -112,11 +114,11 @@ module.exports = {
     backgroundOrigin: 'border-box',
     userSelect: 'none',
     flexShrink: 0,
-    height: '1em',
-    width: '1em',
+    height: defaultTheme.spacing[4],
+    width: defaultTheme.spacing[4],
     color: defaultTheme.colors.blue[500],
     backgroundColor: defaultTheme.colors.white,
-    borderColor: defaultTheme.borderColor.DEFAULT,
+    borderColor: defaultTheme.colors.gray[300],
     borderWidth: defaultTheme.borderWidth.DEFAULT,
     borderRadius: defaultTheme.borderRadius.DEFAULT,
     iconColor: defaultTheme.colors.white,
@@ -124,8 +126,8 @@ module.exports = {
       `<svg viewBox="0 0 16 16" fill="${iconColor}" xmlns="http://www.w3.org/2000/svg"><path d="M5.707 7.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L7 8.586 5.707 7.293z"/></svg>`,
     '&:focus': {
       outline: 'none',
-      boxShadow: defaultTheme.boxShadow.outline,
-      borderColor: defaultTheme.colors.blue[400],
+      boxShadow: defaultTheme.boxShadow.outline, // TODO: Will box-shadow outlines be moved?
+      borderColor: defaultTheme.colors.blue[300],
     },
     '&:checked': {
       borderColor: 'transparent',
@@ -138,6 +140,7 @@ module.exports = {
   radio: {
     appearance: 'none',
     colorAdjust: 'exact',
+    // TODO: Still needed now that IE11 is dropped?
     '&::-ms-check': {
       '@media not print': {
         color: 'transparent', // Hide the check
@@ -152,19 +155,19 @@ module.exports = {
     userSelect: 'none',
     flexShrink: 0,
     borderRadius: '100%',
-    height: '1em',
-    width: '1em',
+    height: defaultTheme.spacing[4],
+    width: defaultTheme.spacing[4],
     color: defaultTheme.colors.blue[500],
     backgroundColor: defaultTheme.colors.white,
-    borderColor: defaultTheme.borderColor.DEFAULT,
+    borderColor: defaultTheme.colors.gray[300],
     borderWidth: defaultTheme.borderWidth.DEFAULT,
     iconColor: defaultTheme.colors.white,
     icon: (iconColor) =>
       `<svg viewBox="0 0 16 16" fill="${iconColor}" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="3"/></svg>`,
     '&:focus': {
       outline: 'none',
-      boxShadow: defaultTheme.boxShadow.outline,
-      borderColor: defaultTheme.colors.blue[400],
+      boxShadow: defaultTheme.boxShadow.outline, // TODO: Will box-shadow outlines be moved?
+      borderColor: defaultTheme.colors.blue[300],
     },
     '&:checked': {
       borderColor: 'transparent',
@@ -172,6 +175,9 @@ module.exports = {
       backgroundSize: '100% 100%',
       backgroundPosition: 'center',
       backgroundRepeat: 'no-repeat',
+    },
+    '&:checked:focus': {
+      borderColor: 'transparent',
     },
   },
 }
